00f4175cb83e699d2c2db3a263b9f5a21aa327ed
Victor <victorhugo.anizau@outlook.com>
2025-05-28 14:18:23 -0300

    `Added auth routes and reordered route usage in index.js`

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require('express'); 
 const router = Router(); 
 
+const authRoutes = require('./auth/index')
 const animalsRoutes = require('./animals'); 
 const usersRoutes = require('./users');
 const vaccinesRoutes = require('./vaccines');
 const vaccineApplicationsRoutes = require('./vaccine_applications');
 
+router.use(authRoutes); 
 router.use(animalsRoutes); 
 router.use(usersRoutes);  
 router.use(vaccinesRoutes); 
