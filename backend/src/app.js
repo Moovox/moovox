4437801d6642f60f7dc57727e86b6f6e7453b21f
Ricardo Augusto <166884977+RicardoAugust-0@users.noreply.github.com>
2025-05-28 09:27:47 -0300

    refactor: reorganizar middleware CORS e remover duplicações

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,26 @@ const routes = require('./routes')
 
 const app = express();
 
+// CORS deve ser o primeiro middleware
+app.use(cors({
+    origin: 'https://moovox.systems',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// Middleware para responder manualmente a OPTIONS
+app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Origin', 'https://moovox.systems');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.header('Access-Control-Allow-Credentials', 'true');
+        return res.sendStatus(204);
+    }
+    next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -16,19 +36,6 @@ app.use((req, res, next) => {
     });
     next();
 });
-
-app.use(cors({
-    origin: 'https://moovox.systems',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
-app.options('*', cors({
-    origin: 'https://moovox.systems',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
 
 app.use('/api', routes);
 
