42c7d573dfb4cefda5608450d75564204c65dc24
RicardoAugust-0 <tatianejc10@gmail.com>
2025-06-06 19:53:17 -0300

    feat: atualizar arquivos de configuração e remover componentes obsoletos
    - Atualizado o arquivo index.html para referenciar novos scripts e estilos.
    - Removidos arquivos JavaScript e CSS desnecessários do diretório de assets, incluindo componentes de alerta, mapas de animais e serviços relacionados.
    - Excluídos arquivos de estilo e scripts que não estavam mais em uso, otimizando a estrutura do projeto.

---
+++ frontend/dist/assets/textarea-Dz3X82ew.js
@@ -0,0 +1,11 @@
+import{c as t,j as a,f as o,r as n}from"./index-5GhjU2z2.js";/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]],u=t("loader-circle",s);/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6",key:"4alrt4"}],["path",{d:"M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2",key:"v07s0e"}],["line",{x1:"10",x2:"10",y1:"11",y2:"17",key:"1uufr5"}],["line",{x1:"14",x2:"14",y1:"11",y2:"17",key:"xtxkd"}]],f=t("trash-2",d);function x({className:e,type:r,...i}){return a.jsx("input",{type:r,"data-slot":"input",className:o("file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground border-input flex h-9 w-full min-w-0 rounded-md border bg-white px-3 py-1 text-base text-gray-800 shadow-sm outline-none transition-all file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm","focus-visible:border-amber-400 focus-visible:ring-[3px] focus-visible:ring-amber-200","aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",e),...i})}const l=n.forwardRef(({className:e,...r},i)=>a.jsx("textarea",{className:o("border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex min-h-[80px] w-full rounded-md border px-3 py-2 text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",e),ref:i,...r}));l.displayName="Textarea";export{x as I,u as L,f as T,l as a};
